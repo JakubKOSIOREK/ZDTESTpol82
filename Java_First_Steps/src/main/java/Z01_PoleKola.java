825184646a62277d1755cfba1b1ecd16038bee18
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-22 11:07:16 +0100

    Update Z01_PoleKola.java

--- Java_First_Steps/src/main/java/Z01_PoleKola.java
+++ Java_First_Steps/src/main/java/Z01_PoleKola.java
@@ -1,2 +1,17 @@
 public class Z01_PoleKola {
+    public static void main(String[] args) {
+
+        //zmienne
+
+        double promienKola = 4;
+
+        //stałe
+
+        final double PI = 3.1416;               //final oznacza stałą
+
+        double poleKola;
+        poleKola = PI * promienKola * promienKola;
+
+        System.out.println("Koło o promieniu = " + promienKola + " ma pole = " + poleKola);
+    }
 }
