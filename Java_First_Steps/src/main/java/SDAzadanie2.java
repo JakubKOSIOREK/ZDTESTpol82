2f12166a32761d04dd3c3f7507d9860471e82da4
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-22 11:24:26 +0100

    update SDAzadanie2.java

--- Java_First_Steps/src/main/java/SDAzadanie2.java
+++ Java_First_Steps/src/main/java/SDAzadanie2.java
@@ -1,6 +1,19 @@
 public class SDAzadanie2 {
     public static void main(String[] args) {
+    /*
+    Napisz program, w którym zainicjalizujesz zmienne typu prymitywnego i przypiszesz do nich następujące wartości:
 
+    byte -> b = 68
+    short -> s = 10000
+    int -> i = 2147483647
+    long -> l = 822337203
+    float -> f = 234.5f
+    double -> d = 123.4
+    boolean -> bo = true
+    string -> st = "Software Development Academy"
+
+    Wartości zmiennych wydrukuj na standardowe wyjście (ekran).
+     */
         byte b = 68;
         short s = 10000;
         int i = 2147483647;
