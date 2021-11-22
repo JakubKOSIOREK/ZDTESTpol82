a92d17dbc80d742d5812cf5320ce2b1e80d1f9f2
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-22 11:04:50 +0100

    Add Z00_HelloWorld.java

---
+++ Java_First_Steps/src/main/java/Z00_HelloWorld.java
@@ -0,0 +1,14 @@
+public class Z00_HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");          //instrukcja wyświetlania/wypisania, tekst zawsze w " "
+        System.out.println("Ala ma kota.");         //instrukcja println przenosi kolejną odpowiedź zawsze do nowej lini
+
+        System.out.println("Wersja z print");
+
+        System.out.print("Hello World");//instrukcja print nie przenosi kolejnej odpowiedzi do nowej lini
+        System.out.println("Ala ma kota");
+
+    }   // koniec programu
+}      // koniec ciała
