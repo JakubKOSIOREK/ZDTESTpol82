929e2ab7af05bf15ba139a020b6874ac1d8439ad
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-22 11:21:04 +0100

    add SDAzadanie2.java

---
+++ Java_First_Steps/src/main/java/SDAzadanie2.java
@@ -0,0 +1,22 @@
+public class SDAzadanie2 {
+    public static void main(String[] args) {
+
+        byte b = 68;
+        short s = 10000;
+        int i = 2147483647;
+        long l = 822337203;
+        float f = 234.5f;
+        double d = 123.4;
+        boolean bo = true;
+        String st = "Software Development Academy";
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bo);
+        System.out.println(st);
+    }
+}
