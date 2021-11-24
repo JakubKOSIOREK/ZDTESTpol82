959bc467491267479461a66710127ddecb929483
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-24 22:32:13 +0100

    update files

--- Java_First_Steps/src/main/java/ZadaniaSDA/SDAzadanie04.java
+++ Java_First_Steps/src/main/java/ZadaniaSDA/SDAzadanie04.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 
 public class SDAzadanie04 {
     public static void main(String[] args) {
+    /*
+    Napisz program, który pobierze od użytkownika liczbę całkowitą i wypisze ją na ekran.
+    Na ekranie powinien wyświetlać się następujący komunikat "Użytkownik wprowadził X",
+    gdzie X jest liczbą podaną przez użytkownika.
+    Zakładamy sytuację, w której użytkownik, nie będzie wprowadzał innych typów danych niż liczby całkowite.
+
+    Do obsługi wprowadzania danych przez użytkownika zastosuj bibliotekę Scanner.
+    Wykonaj testy, sprawdź jak program, zachowuje się dla innych typów danych.
+ */
+
+
 
         Scanner klawiatura = new Scanner (System.in);
         System.out.println("Podaj dowolną liczbę całkowitą X :");
-        int liczbaCalkowita = klawiatura.nextInt();
+        int x = klawiatura.nextInt();
 
 
-        System.out.println("\nUżytkownik wprowadził : " + liczbaCalkowita);
+        System.out.println("\nUżytkownik wprowadził : " + x);
 
     }
 }
--- Java_First_Steps/src/main/java/ZadaniaSDA/SDAzadanie05.java
+++ Java_First_Steps/src/main/java/ZadaniaSDA/SDAzadanie05.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class SDAzadanie05 {
     public static void main(String[] args) {
+        /*
+        Napisz program, który pobiera dwie liczby całkowite, a następnie wykonuje następujące operacje matematyczne:
+        - dodawanie
+        - odejmowanie
+        - mnożenie
+        - dzielenie
+        - podniesienie do potęgi
+            (pierwszy argument to liczba, która będzie podnoszona do potęgi,
+            drugi argument to potęga, do której podnosimy liczbę)
+
+        Wynik operacji powinien zostać przypisany do nowo utworzonej zmiennej, odpowiedniego typu.
+
+        Wynik powinien zostać wyświetlony na ekranie, w następującej postaci:
+        - dodawanie -> "a = A, b = B, wynik to C"
+        - odejmowanie -> "a = A, b = B, wynik to C"
+        - mnożenie -> "a = A, b = B, wynik to C"
+        - dzielenie -> "a = A, b = B, wynik to C"
+        - podniesienie do potęgi -> "a = A, podniesione do potęgi B, daje wynik C"
+         */
 
         Scanner klawiatura = new Scanner(System.in);
 
@@ -34,7 +53,7 @@ public class SDAzadanie05 {
         System.out.println("a = " + a + ", b = " + b + ", wynik a / b = " + iloraz);
 
         // potęgowanie
-        double wynik_potegowania = Math.pow(a, b);       // tu stosujemy klasę matematyczną Math z metodą .pow
+        double wynik_potegowania = Math.pow(a, b);       // tu stosujemy klasę matematyczną Math z metodą .pow()
         System.out.println("a = " + a + ", b = " + b + ", wynik a + b = " + wynik_potegowania);
 
 
