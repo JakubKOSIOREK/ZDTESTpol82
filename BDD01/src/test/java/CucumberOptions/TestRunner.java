5661d952dfa76f7dee958415a510dfb427f65314
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-25 19:17:59 +0100

    add BDD

---
+++ BDD01/src/test/java/CucumberOptions/TestRunner.java
@@ -0,0 +1,10 @@
+package CucumberOptions;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)  // trzeba dodać odnośnik do tego co w pom.xml
+
+
+public class TestRunner {
+}
