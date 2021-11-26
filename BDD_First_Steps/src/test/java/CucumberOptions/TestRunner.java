ee6b9adadd682ba23a99e3fbe50bce8b14e64489
JakubKosiorek <jakubkosiorek@gmail.com>
2021-11-26 12:31:37 +0100

    Step 6: update TestRunner.java
    konfigurujemy klasę TestRunner.java:
    - podłączamy niezbędne biblioteki z pliku pom.xml
    - podłączamy źródła z których klasa będzie korzystała
    
    we configure the TestRunner.java class:
    - we connect the necessary libraries from the pom.xml file
    - we connect the sources that the class will use

--- BDD_First_Steps/src/test/java/CucumberOptions/TestRunner.java
+++ BDD_First_Steps/src/test/java/CucumberOptions/TestRunner.java
@@ -1,4 +1,16 @@
 package CucumberOptions;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)        // podłączenie biblioteki cucumber z pliku pom.xml
+
+@CucumberOptions(               // podłączamy źródła dla klasy TestRunner
+
+        features = "src/test/java/Features",    // ścieżka do katalogu zawierającego historyjki użytkownika
+        glue = "StepDefinitions"                // ścieżka do katalogu zawierającego kroki Given->When->Then
+)
+
 public class TestRunner {
 }
